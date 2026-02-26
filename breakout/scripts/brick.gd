57684bd416f305ee98a8e88e81cc5a9c8a87cd92
Kiryl Siaukovich <k.siaukovich@gmail.com>
2026-02-26 11:23:39 +0100

    Changed orientation to landscape, colored bricks

--- breakout/scripts/brick.gd
+++ breakout/scripts/brick.gd
@@ -3,12 +3,19 @@ class_name Brick extends RigidBody2D
 const scene: PackedScene = preload("res://scenes/brick.tscn")
 const pixels_per_meter := 16
 
-static func create(size: Vector2) -> Brick:
+var _mesh_color: Color
+
+static func create(size: Vector2, color: Color) -> Brick:
 	var brick: Brick = scene.instantiate()
 
 	brick.set_size(size)
 	
+	brick._mesh_color = color
+	
 	return brick
+
+func _ready() -> void:
+	$MeshInstance2D.modulate = _mesh_color
 
 func set_size(value: Vector2) -> void:
 	var shape = RectangleShape2D.new()
--- breakout/scripts/bricks.gd
+++ breakout/scripts/bricks.gd
@@ -1,19 +1,27 @@
 extends Node
 
-@export var rows := 8
-@export var colums := 5
-@export var h_spacing := 10
-@export var v_spacing := 10
-@export var brick_width := 150
-@export var brick_height := 50
+signal bricks_cleared
 
-var brickScene = preload("res://scenes/brick.tscn")
+var rows := 8
+var colums := 25
+var h_spacing := 10
+var v_spacing := 10
+var brick_width := 100
+var brick_height := 50
+
+var brick_count: int:
+	get: return brick_count
+	set(value):
+		brick_count = value
+		if brick_count == 0:
+			bricks_cleared.emit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_create_bricks()
 
 func _create_bricks() -> void:
+	brick_count = rows * colums
 	var total_width = colums * brick_width + (colums - 1) * h_spacing
 	var total_height = rows * brick_height + (rows - 1) * v_spacing
 	
@@ -24,19 +32,22 @@ func _create_bricks() -> void:
 	
 	var start_x = (get_viewport().get_visible_rect().size.x - total_width) / 2
 	var start_y := 100
-	var current_column := 0
 	var brick_size := Vector2(brick_width, brick_height)
-	while current_column <= colums:
-		var current_row := 0
-		while current_row <= rows:
-			var brick := Brick.create(brick_size)
+	var row_color: Color
+	for current_row in range(rows):
+		if current_row % 2 == 0:
+			row_color = Color(randf(), randf(), randf())
+			print(row_color)
+			
+		for current_column in range(colums):
+			var brick := Brick.create(brick_size, row_color)
 			brick.position = Vector2(
 				start_x + current_column * (brick_width + h_spacing),
 				start_y + current_row * (brick_height + v_spacing))
 			
-			print(brick.position)
-			
 			add_child(brick)
-			current_row += 1
 		
-		current_column += 1
+		
+func remove_brick(brick: Brick):
+	brick.queue_free()
+	brick_count -= 1
